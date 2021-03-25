49bc3c146d7a617e86b40a3c03730761e2bbe6de
Damali Boyd <damaliboyd@ymail.com>
2021-03-25 09:19:25 -0400

    Created Seed Data Created Context Added seed data into program.cs

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Course>().ToTable("Course");
+            builder.Entity<Enrollment>().ToTable("Enrollment");
+            builder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
